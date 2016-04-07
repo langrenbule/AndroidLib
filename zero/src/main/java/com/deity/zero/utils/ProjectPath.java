97054d0a028d38848cbd691dfe14fb0d05e59a19
langrenbule <546024423@qq.com>
2016-04-07 22:53:00 +0800

    (*) 获取SD卡根目录的方法

--- zero/src/main/java/com/deity/zero/utils/ProjectPath.java
+++ zero/src/main/java/com/deity/zero/utils/ProjectPath.java
@@ -1,9 +1,16 @@
 package com.deity.zero.utils;
 
+import android.os.Environment;
+
 /**
  * 这是应用的一些目录地址
  * Created by Deity on 2016/4/7.
  */
+@SuppressWarnings("ALL")
 public class ProjectPath {
 
+    /**获取SD卡的目录*/
+    public void getSDRoot(){
+        Environment.getExternalStorageDirectory();
+    }
 }
